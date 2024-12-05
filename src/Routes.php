76d3b818e5bec600f796e44df29017c97de70730
Tim Schwartz <tim@metaverse.systems>
2024-12-04 19:51:42 -0600

    Update composer.json dependencies and change API route prefix to 'api/clarion-app/gtd'

--- src/Routes.php
+++ src/Routes.php
@@ -5,7 +5,7 @@ use ClarionApp\GettingThingsDone\Controllers\ActionController;
 use ClarionApp\GettingThingsDone\Controllers\ProjectController;
 use ClarionApp\GettingThingsDone\Controllers\ContextController;
 
-Route::group(['middleware'=>['auth:api'], 'prefix'=>'api/metaverse-systems/gtd' ], function () {
+Route::group(['middleware'=>['auth:api'], 'prefix'=>'api/clarion-app/gtd' ], function () {
     Route::apiResource('actions', ActionController::class);
     Route::apiResource('projects', ProjectController::class);
     Route::apiResource('contexts', ContextController::class);
